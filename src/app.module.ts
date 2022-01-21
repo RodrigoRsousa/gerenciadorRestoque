09204e442472463c3d1b064a6eab589a68c78189
Rodrigo Reis de Sousa <32500158+RodrigoRsousa@users.noreply.github.com>
2022-01-20 22:23:39 -0300

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProductsModule, UsersModule, AuthModule],
+  imports: [ProductsModule, UsersModule, AuthModule, UserModule],
   controllers: [],
   providers: [],
 })
